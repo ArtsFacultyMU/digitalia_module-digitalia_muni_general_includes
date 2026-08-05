6b37b83e4824f37169a076208060c19b4654ff47
krejcir <krejcir@ics.muni.cz>
2026-08-05 16:39:13 +0200

    Add special style support for lan dropdown switcher.
    This adds "appearance: button" in the select style, needed to display
    ending "v" character at the end of the lang switcher.

--- js/language_switcher_update.js
+++ js/language_switcher_update.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
   let links = document.querySelectorAll("#block-islandora-muni-languageswitcher a");
+  
+  // language switche drop down
+  let lang_form = document.getElementById("lang_dropdown_form_lang-dropdown-form");
+  if (lang_form) {
+    const url = new URL(lang_form.action, window.location.origin);
+    url.searchParams.set("languageswitch", "1");
+    lang_form.action = url.toString();
+  }
+  if (navigator.userAgent.toLowerCase().includes("firefox")) {
+    let lang_select = document.getElementById("edit-lang-dropdown-select");
+    lang_select.style.appearance = "button";
+  }
+
 
   // arnenovak cinematicbrno and digilib have different block id
   if (links.length == 0) {
@@ -15,4 +28,5 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     link.href = url.toString(); 
   });
+
 });
